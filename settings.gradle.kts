781fd0c05eaf088f4f665e3640769b7f12263c3e
kqueens196-pixel <kqueens196@gmail.com>
2026-09-05 04:36:38 +0530

    Bypass 429 by enforcing Google repository and dependency refresh

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 pluginManagement {
     repositories {
         google()
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
@@ -8,6 +9,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        google()
         google()
         mavenCentral()
     }
